c11910652252136b720891c6369fda727d4bc009
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2024-07-21 09:44:22 +0200

    js

---
+++ JS/variable.js
@@ -0,0 +1,29 @@
+// "use srict"
+let myAge ="13";
+console.log(myAge);
+const myName="Mykola";
+console.log(myName);
+const isStudent = true;
+console.log(isStudent);
+const myString = "I Always Come Back";
+console.log( myString);
+const myNumber = "4";
+console.log(myNumber);
+const number = myNumber + 10;
+console.log(number);
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім'я");
+console.log(userName);
+
+const question = confirm("Ви впевнені за використання?");
+if (question == true) {
+  const accept = alert("Дякую за підтвердження");
+}
+
+const messange = alert("Обережно, небезпечна дія!");
+const choice = confirm("Підтвердити виконання дії");
+if (question == true) {
+  const accept = alert("Дякую за підтвердження");
+}
